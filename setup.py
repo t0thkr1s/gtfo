a94f1214e425927226de75020e5e97f1be276b4f
t0thkr1s <t0thkr1s@icloud.com>
2020-01-08 11:33:13 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='gtfo',
+    version='1.0',
+    url='https://medium.com/infosec-adventures',
+    author='https://github.com/t0thkr1s',
+    author_email='t0thkr1s@icoud.com',
+    description='Command-line program for GTFOBins. It helps you to bypass system security restrictions.',
+    install_requires=['colorama', 'pygments']
+)
